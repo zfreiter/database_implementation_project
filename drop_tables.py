34ed3d87d4d94bfeeeb973b499819ba5cc30641a
m-istvan <56458289+m-istvan@users.noreply.github.com>
2022-05-29 17:02:00 -0700

    Added drop_tables function

---
+++ drop_tables.py
@@ -0,0 +1,20 @@
+import psycopg2
+import os
+from db import *
+
+def drop_tables():
+    drop_table("company")
+    drop_table("film")
+    drop_table("genre")
+    drop_table("persons")
+    drop_table("film_company")
+    drop_table("film_genre")
+    drop_table("film_persons")
+    drop_table("rating")
+
+def drop_table(table):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS %s;",(table))
+    conn.commit()
+    conn.close()
